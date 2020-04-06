5cfc3173f2370d9eb36066b8c4a0c363629fc105
Valy Dia <2129101+valydia@users.noreply.github.com>
2020-04-06 19:12:52 +0100

    [WIP] Step 2: Write specs

--- src/test/scala/com/example/CheckoutSpec.scala
+++ src/test/scala/com/example/CheckoutSpec.scala
@@ -18,6 +18,7 @@ class CheckoutSpec extends AnyFeatureSpec with Matchers with GivenWhenThen {
       Then("The total coast should be £0")
       result shouldBe 0
     }
+
     Scenario("1 - Shopping Cart") {
       Given("A list of items [ Apple, Apple, Orange, Apple ]")
       val items = List("Apple", "Apple", "Orange", "Apple")
@@ -25,8 +26,30 @@ class CheckoutSpec extends AnyFeatureSpec with Matchers with GivenWhenThen {
       When("Calculating the total cost")
       val result = Checkout.checkout(items)
 
-      Then("The total coast should be £2.05")
-      result shouldBe 2.05
+      Then("The total coast should be £1.45")
+      result shouldBe 1.45
+    }
+
+    Scenario("2 - Simple offers") {
+      Given(
+        "A list of items [ Apple, Apple, Orange, Apple, Orange, Orange, Apple, Orange ]"
+      )
+      val items = List(
+        "Apple",
+        "Apple",
+        "Orange",
+        "Apple",
+        "Orange",
+        "Orange",
+        "Apple",
+        "Orange"
+      )
+
+      When("Calculating the total cost")
+      val result = Checkout.checkout(items)
+
+      Then("The total coast should be £1.95")
+      result shouldBe 1.95
     }
 
   }
