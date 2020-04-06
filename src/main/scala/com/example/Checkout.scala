24e4d37f6d6a3638a879cf77368400e0145e5b31
Valy Dia <2129101+valydia@users.noreply.github.com>
2020-04-06 19:58:24 +0100

    Complete step 2

--- src/main/scala/com/example/Checkout.scala
+++ src/main/scala/com/example/Checkout.scala
@@ -2,23 +2,48 @@ package com.example
 
 object Checkout {
 
-  type Item  = String
   type Price = BigDecimal
 
-  val priceMap = Map("Apple" -> BigDecimal(0.60), "Orange" -> BigDecimal(0.25))
+  trait Item {
+    val name: String
+    val price: Price
+    val discountQuantity: Int
+    val discountedPrice: Price
+  }
+  case object Apple extends Item {
+    override val name: String           = "Apple"
+    override val price: Price           = BigDecimal(0.60)
+    override val discountQuantity: Int  = 2
+    override val discountedPrice: Price = price
+  }
 
-  def checkout(items: List[Item]): Price = {
+  case object Orange extends Item {
+    override val name: String           = "Orange"
+    override val price: Price           = BigDecimal(0.25)
+    override val discountQuantity: Int  = 3
+    override val discountedPrice: Price = 2 * price
+  }
+  object Item {
+    def fromString(string: String): Option[Item] =
+      string match {
+        case "Apple"  => Some(Apple)
+        case "Orange" => Some(Orange)
+        case _        => None
+      }
+  }
+
+  def checkout(items: List[String]): Price = {
     val itemToCount =
       items
-        .groupBy(identity)
-        .view
-        .mapValues(_.length)
-        .toMap
+        .groupBy(Item.fromString)
+        .collect { case (Some(item), list) => item -> list.length } // Discard invalid input
 
     itemToCount.foldLeft(BigDecimal(0)) {
       case (acc, (item, count)) =>
-        // We assume there is always a for a given item
-        acc + priceMap(item) * count
+        val appliedDiscountPrice = (count / item.discountQuantity) * item.discountedPrice
+        val notDiscountedPrice   = (count % item.discountQuantity) * item.price
+        val itemPrice            = appliedDiscountPrice + notDiscountedPrice
+        acc + itemPrice
     }
   }
 
